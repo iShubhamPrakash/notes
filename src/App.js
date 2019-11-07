7af04a6e271df71e65cf2d75128e4a5c789ad469
Shubham Prakash <shubham.prakash2308@gmail.com>
2019-11-08 03:47:08 +0530

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Notes from './Notes';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Notes/>
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+  <div className="header">
+    <span className="logo"><i className="far fa-sticky-note"> </i> <h1>Notes</h1><small><a href="https://shubhamprakash.me" rel="noopener noreferrer" target="_blank">By Shubham Prakash</a></small></span>
+    <div className="streatch"></div>
+    <div className="social">
+      <a href="https://linkedin.com/in/ishubhamprakash/" rel="noopener noreferrer" target="_blank">
+      <i className="fab fa-linkedin-in linkedin"></i>
+      </a>
+      <a href="https://github.com/i-shubhamprakash" rel="noopener noreferrer" target="_blank">
+      <i className="fab fa-github github"></i>
+      </a>
+      <a href="https://twitter.com/iSuvm" rel="noopener noreferrer" target="_blank">
+      <i className="fab fa-twitter twitter"></i>
+      </a>
+      <a href="https:shubhamprakash.me" rel="noopener noreferrer" target="_blank">
+        <i className="fas fa-globe globe"></i>
+      </a>
+    </div>
+  </div>
+  )
+}
