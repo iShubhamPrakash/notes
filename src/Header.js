9ff990309c40be30556b76d5f35c9cadd2e7fbe4
Shubham Prakash <shubham.prakash2308@gmail.com>
2019-11-08 06:33:33 +0530

    Add icons

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
   <div className="header">
-    <span className="logo"><i className="far fa-sticky-note"> </i> <h1>Notes</h1><small><a href="https://shubhamprakash.me" rel="noopener noreferrer" target="_blank">By Shubham Prakash</a></small></span>
+    <span className="logo"><img className="logo-img" src="/assets/note-icon.svg"/> <h1>Notes</h1><small><a href="https://shubhamprakash.me" rel="noopener noreferrer" target="_blank">By Shubham Prakash</a></small></span>
     <div className="streatch"></div>
     <div className="social">
       <a href="https://linkedin.com/in/ishubhamprakash/" rel="noopener noreferrer" target="_blank">
